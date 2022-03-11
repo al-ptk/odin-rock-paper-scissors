f3b78bc81e0040a9fc89ec2411511321bf7804c3
Alan Patrick <alan.patrick.fv@gmail.com>
2022-03-11 14:43:41 -0300

    Function playRound done.

--- script.js
+++ script.js
@@ -12,11 +12,43 @@ function computerPlay () {
 }
 
 function playRound (playerSelection, computerSelection) {
-    // You either win, lose or tie.
+
     let ps = playerSelection.toLowerCase();
     let cs = computerSelection.toLowerCase();
 
+    //First test for tie
     if (ps === cs) {
-        return `You tie! ${ps}`
+        return `You tie! ${upFirst(ps)} is equal to ${upFirst(cs)}`;
+    } else {
+
+        if (ps === 'rock') {
+            if (cs === 'scissors'){
+                return `You win! ${upFirst(ps)} beats ${upFirst(cs)}!`
+            } else {
+                return `You lose! ${upFirst(ps)} gets beaten by ${upFirst(cs)}!`
+            }
+        }
+
+        else if (ps === 'paper') {
+            if (cs === 'rock'){
+                return `You win! ${upFirst(ps)} beats ${upFirst(cs)}!`
+            } else {
+                return `You lose! ${upFirst(ps)} gets beaten by ${upFirst(cs)}!`
+            }
+        }
+
+        else if (ps === 'scissors') {
+            if (cs === 'paper'){
+                return `You win! ${upFirst(ps)} beats ${upFirst(cs)}!`
+            } else {
+                return `You lose! ${upFirst(ps)} gets beaten by ${upFirst(cs)}!`
+            }
+        }
     }
+    return "Something went wrong";
 }
+
+
+// Testing one round
+//let pick = prompt("Rock, Paper or Scissors?");
+p(playRound('paper', 'rock'));
