f2e436edfbd499e770c585b2e8f282badb43b750
Alan Patrick <alan.patrick.fv@gmail.com>
2022-03-14 17:30:04 -0300

    Rps-ui branch with a MVC architeture.

---
+++ model.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// Helper functions
+let p = (arg) => console.log(arg);
+
+// Capitilize first letter string
+let capFirst = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+
+// Main functions
+function computerPlay () {
+    const signs = ['rock','paper','scissors'];
+    // This is how you pick an item randomly from an array in JS
+    return signs[Math.floor(Math.random() * signs.length)];
+}
+
+
+function playRound (playerSelection, computerSelection) {
+    let ps = playerSelection.toLowerCase();
+    let cs = computerSelection.toLowerCase();
+    //First test for tie
+    if (ps === cs) {
+        return `You tie! ${capFirst(ps)} is equal to ${capFirst(cs)}`;
+    } else {
+        //Then test for each possible player input
+        if (ps === 'rock') {
+            if (cs === 'scissors'){
+                return `You win! ${capFirst(ps)} beats ${capFirst(cs)}!`
+            } else {
+                return `You lose! ${capFirst(ps)} gets beaten by ${capFirst(cs)}!`
+            }
+        }
+        else if (ps === 'paper') {
+            if (cs === 'rock'){
+                return `You win! ${capFirst(ps)} beats ${capFirst(cs)}!`
+            } else {
+                return `You lose! ${capFirst(ps)} gets beaten by ${capFirst(cs)}!`
+            }
+        }
+        else if (ps === 'scissors') {
+            if (cs === 'paper'){
+                return `You win! ${capFirst(ps)} beats ${capFirst(cs)}!`
+            } else {
+                return `You lose! ${capFirst(ps)} gets beaten by ${capFirst(cs)}!`
+            }
+        }
+    }
+    return `${playerSelection} is invalid.`;
+}
+
+
+function calcResult () {
+    return playRound( prompt("Rock, paper or scissors", computerPlay()), computerPlay());
+}
+
+
+function updateScore (score, result) {
+    let res = result.slice(0,5);
+
+    if (res === "You w") { // Player wins
+        score[0] += 1;
+    } else if (res === "You l") { // Computer wins
+        score[1] += 1;
+    } 
+}
+
+
+function showScore(score) {
+    p(`Score > Player: ${score[0]} | Computer ${score[1]}`);
+}
+
